b4bef83132009b58646c8176aa39664d5a5eab6a
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-05-21 16:19:22 +0300

    Update models.py

--- saucy/cart/models.py
+++ saucy/cart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Cart of {self.user.username}'
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.product.name} in {self.cart.user.username}\'s cart'
