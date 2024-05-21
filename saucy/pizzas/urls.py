a07e14b99395106c487ddf46578484555c8b06ba
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-05-21 16:49:06 +0300

    Update urls.py

--- saucy/pizzas/urls.py
+++ saucy/pizzas/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-
+urlpatterns = [
+    path('', views.pizza_list, name='pizza_list'),
+    path('<int:pizza_id>/', views.pizza_detail, name='pizza_detail'),
+]
