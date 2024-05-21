0d301e976e640d3dcb3a2f55e29be7571ef8d000
Fiona26 <fionamuthoni18@gmail.com>
2024-05-21 11:10:06 +0300

    added pizza

--- saucy/pizzas/models.py
+++ saucy/pizzas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    name = models.CharField(max_length=100)
+    product_id = models.CharField(max_length=10, unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- saucy/pizzas/tests.py
+++ saucy/pizzas/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Pizza
 
 # Create your tests here.
+class PizzaModelTest(TestCase):
+    def test_pizza_creation(self):
+        pizza = Pizza.objects.create(name="Margherita Pizza", product_id="PIZZA001", price=9.99)
+        self.assertEqual(str(pizza), pizza.name)
+
