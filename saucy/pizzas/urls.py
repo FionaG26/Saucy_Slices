0f36a07e82744a9c13e65644a348500b163d079f
Fiona26 <fionamuthoni18@gmail.com>
2024-05-21 15:00:02 +0300

    added

--- saucy/pizzas/urls.py
+++ saucy/pizzas/urls.py
@@ -1,7 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('pizza/<str:id>/', views.pizza_detail, name='pizza_detail'),
-]
+
--- saucy/pizzas/views.py
+++ saucy/pizzas/views.py
@@ -2,13 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    # Logic to fetch all available pizzas
-    pizzas = Pizza.objects.all()
-    return render(request, 'pizza/home.html', {'pizzas': pizzas})
 
-
-def pizza_detail(request, id):
-     # Logic to fetch details of a specific pizza
-     pizza = Pizza.objects.get(id=id)
-     return render(request, 'pizza/detail.html', {'pizza': pizza})
