60b293afa0ca65be356b211d704da4233e708636
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-05-21 16:42:16 +0300

    Update models.py

--- saucy/orders/models.py
+++ saucy/orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from pizza.models import Pizza
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Order #{self.pk}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.pizza.name} in Order #{self.order.pk}'
