85555f4a9948ccf4013a7ec4de7961c0294a3b47
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-05-21 16:14:30 +0300

    Update urls.py

--- saucy/cart/urls.py
+++ saucy/cart/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+    path('add/', views.add_to_cart_view, name='add_to_cart'),
+    path('update/', views.update_cart_view, name='update_cart'),
+    path('remove/<int:item_id>/', views.remove_from_cart_view, name='remove_from_cart'),
     path('checkout/initiate/', views.initiate_checkout_view, name='initiate_checkout'),
     path('checkout/place-order/', views.place_order_view, name='place_order'),
     path('order/<int:order_id>/status/', views.order_status_view, name='order_status'),
