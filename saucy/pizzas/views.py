a79933e2ac1e837c984357683049a3d860c6e1ce
Fiona Githaiga <111144758+FionaG26@users.noreply.github.com>
2024-05-21 16:50:50 +0300

    Update views.py

--- saucy/pizzas/views.py
+++ saucy/pizzas/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Pizza
 
-# Create your views here.
+def pizza_list(request):
+    pizzas = Pizza.objects.all()
+    # Serialize pizza data
+    return JsonResponse({'pizzas': list(pizzas.values())}, status=200)
 
+def pizza_detail(request, pizza_id):
+    try:
+        pizza = Pizza.objects.get(pk=pizza_id)
+        # Serialize pizza data
+        return JsonResponse({'pizza': pizza}, status=200)
+    except Pizza.DoesNotExist:
+        return JsonResponse({'error': 'Pizza not found'}, status=404)
